6afa8539d58688630ca4093dd8d8221022928787
Beth Medina <medina.beth@gmail.com>
2016-02-29 18:47:38 -0700

    remove duplicate code

--- app/helpers/family_members_helper.rb
+++ app/helpers/family_members_helper.rb
@@ -6,24 +6,6 @@ module FamilyMembersHelper
     options
   end
 
-  def get_parents
-    @family_member.parent != nil ? @family_member.parent : nil
-  end
-
-  def get_children
-    @family_member.children != [] ? @family_member.children : []
-  end
-
-  def get_grandparents
-    puts "************* #{@family_member.parent} *****"
-    @family_member.grandparent != nil ? @family_member.grandparent : nil
-  end
-
-  def get_grandchildren
-    return [] if @family_member.grandchildren == nil
-    @family_member.grandchildren != [] ? @family_member.grandchildren : []
-  end
-
   def count_relationships
     @family_member.descendants.count + @family_member.ancestors.count
   end
